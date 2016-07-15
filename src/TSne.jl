33dba7dd130b2cbfc1274e44e0bbc01b6a338fae
Alexey Stukalov <astukalov@gmail.com>
2016-07-15 11:08:42 +0200

    extend tsne() docstring

--- src/TSne.jl
+++ src/TSne.jl
@@ -108,11 +108,27 @@ function pca{T}(X::Matrix{T}, ndims::Integer = 50)
     return Y
 end
 
-"""
-    Apply t-SNE to `X`, i.e. embed its points into `ndims` dimensions
-    preserving close neighbours.
+"""`tsne(X::Matrix, ndims::Integer=2, reduce_dims::Integer=0, max_iter::Integer=1000)`
+
+    Apply t-SNE (t-Distributed Stochastic Neighbor Embedding) to `X` dataset,
+    i.e. embed its points (rows) into `ndims` dimensions preserving close neighbours.
+
     Different from the orginal implementation,
     the default is not to use PCA for initialization.
+
+    * `reduce_dims` the number of the first dimensions of `X` PCA to use for t-SNE,
+      if 0, all available dimension are used
+    * `pca_init` whether to use the first `ndims` of `X` PCA as the initial t-SNE layout,
+      if `false` (the default), the method is initialized with the random layout
+    * `max_iter` how many iterations of t-SNE to do
+    * `perplexity` the number of "effective neighbours" of a datapoint,
+      typical values are from 5 to 50, the default is 30
+    * `verbose` output informational and diagnostic messages
+    * `progress` display progress meter during t-SNE optimization
+    * `min_gain`, `eta`, `initial_momentum`, `final_momentum`, `momentum_switch_iter`,
+      `stop_cheat_iter`, `cheat_scale` low-level parameters of t-SNE optimization
+
+    See also [Original t-SNE implementation](https://lvdmaaten.github.io/tsne).
 """
 function tsne(X::Matrix, ndims::Integer = 2, reduce_dims::Integer = 0, max_iter::Integer = 1000, perplexity::Number = 30.0;
               min_gain::Number = 0.01, eta::Number = 200.0, pca_init::Bool = false,
