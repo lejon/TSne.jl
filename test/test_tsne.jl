ad5a305368820e3adba191cfbd98f8de02053821
Alexey Stukalov <astukalov@gmail.com>
2017-03-07 12:58:14 +0100

    remove sub-sub @testset grouping

--- test/test_tsne.jl
+++ test/test_tsne.jl
@@ -1,38 +1,26 @@
 @testset "tsne()" begin
     @testset "API tests" begin
-        iris = dataset("datasets","iris")
+        iris = dataset("datasets", "iris")
         X = convert(Matrix, iris[:, 1:4])
-        @testset "verbose=true" begin
-            Y = tsne(X, 2, -1, 10, 15, verbose=true)
-            @test size(Y) == (150, 2)
-        end
-        @testset "verbose=false" begin
-            Y = tsne(X, 3, -1, 10, 15, verbose=false)
-            @test size(Y) == (150, 3)
-        end
-        @testset "no progress bar" begin
-            tsne(X, 2, -1, 10, 15, verbose=true, progress=false)
-        end
-        @testset "no progress bar, verbose=false" begin
-            tsne(X, 2, -1, 10, 15, verbose=false, progress=false)
-        end
-        @testset "PCA for initial layout" begin
-            Y = tsne(X, 2, -1, 10, 15, pca_init=true, cheat_scale=1.0, progress=false)
-            @test size(Y) == (150, 2)
-        end
+        Y = tsne(X, 2, -1, 10, 15, verbose=true)
+        @test size(Y) == (150, 2)
+        Y = tsne(X, 3, -1, 10, 15, verbose=false)
+        @test size(Y) == (150, 3)
+        tsne(X, 2, -1, 10, 15, verbose=true, progress=false)
+        tsne(X, 2, -1, 10, 15, verbose=false, progress=false)
+        Y = tsne(X, 2, -1, 10, 15, pca_init=true, cheat_scale=1.0, progress=false)
+        @test size(Y) == (150, 2)
     end
 
     @testset "Iris dataset" begin
-        iris = dataset("datasets","iris")
+        iris = dataset("datasets", "iris")
         X = convert(Matrix, iris[:, 1:4])
-        @testset "embed in 3D" begin
-            Y = tsne(X, 3, -1, 1500, 15, progress=false)
-            @test size(Y) == (150, 3)
-        end
-        @testset "embed in 2D" begin
-            Y = tsne(X, 2, 50, 50, 20, progress=false)
-            @test size(Y) == (150, 2)
-        end
+        # embed in 3D
+        Y3d = tsne(X, 3, -1, 1500, 15, progress=false)
+        @test size(Y3d) == (150, 3)
+        # embed in 2D
+        Y2d = tsne(X, 2, 50, 50, 20, progress=false)
+        @test size(Y2d) == (150, 2)
     end
 
     @testset "MNIST.traindata() dataset" begin
